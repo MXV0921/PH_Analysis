064a6bcba4b75695446f68374b29af6701a96c90
Mark Vogel <mxv0921@gmail.com>
2022-05-23 21:04:51 -0400

    upload complete SQL Challenge code

---
+++ Query/Employee_Database_challenge.sql
@@ -0,0 +1,43 @@
+Drop table challenge_table cascade;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO challenge_table
+FROM employees as e
+LEFT JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC;
+
+Select * FROM challenge_table
+
+-- Use Dictinct with Orderby to remove duplicate rows
+
+SELECT DISTINCT ON (ct.emp_no) ct.emp_no,
+ct.first_name,
+ct.last_name,
+ct.title
+
+INTO unique_titles
+FROM challenge_table AS ct
+WHERE (ct.to_date BETWEEN '9999-01-01' AND '9999-01-01')
+ORDER BY ct.emp_no, ct.from_date DESC;
+
+SELECT * FROM unique_titles
+
+
+DROP TABLE retiring_titles
+
+--COUNT THE NUMBER OF RETIREES BY JOB TITLE
+SELECT COUNT (ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title 
+ORDER BY COUNT DESC
+
+SELECT * FROM retiring_titles
