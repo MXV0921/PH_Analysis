0613dba3e3353cf12812e2b4757f1e3a79c593c2
Mark Vogel <mxv0921@gmail.com>
2022-05-26 13:58:45 -0400

    upload PNG files

--- Query/Employee_Database_challenge.sql
+++ Query/Employee_Database_challenge.sql
@@ -41,3 +41,24 @@ GROUP BY ut.title
 ORDER BY COUNT DESC
 
 SELECT * FROM retiring_titles
+
+--DELIVERABLE 2
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_availability
+FROM employees AS e
+LEFT JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date BETWEEN '9999-01-01' AND '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
+
+SELECT * FROM mentorship_availability
